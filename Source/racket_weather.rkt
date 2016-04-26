45afb0149ba8ddf1fa809e30fdb45241842fd17b
Huy Huynh <Huy_Huynh1@student.uml.edu>
2016-04-26 19:39:45 -0400

    Update racket_weather.rkt
    Plotting the line graphs

--- Source/racket_weather.rkt
+++ Source/racket_weather.rkt
@@ -5,6 +5,13 @@
 ;; Call the get_weather procedure
 (get_weather cities_list get_list)
 
+;; Generate the plots for all five cities
+(gen-graph list-of-json-files "Boston")                ;; Boston
+(gen-graph (cdr list-of-json-files) "Lawrence")        ;; Lawrence
+(gen-graph (cddr list-of-json-files) "Lowell")         ;; Lowell
+(gen-graph (cdddr list-of-json-files) "Worcester")     ;; Worcester
+(gen-graph (cddddr list-of-json-files) "Manchester")   ;; Manchester
+
 ;; Now try and run Huy's Viz program.
 ;; Use another require, and make another provide-all-defined
 ;; in Huy / JT's Viz programs so that we can do all the plotting in
