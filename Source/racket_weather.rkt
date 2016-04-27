64bbadb2260fe4238d1b7b71d4e5337fe0b90345
Huy Huynh <Huy_Huynh1@student.uml.edu>
2016-04-26 23:28:49 -0400

    Update racket_weather.rkt
    little update to make the graphs plot horizontally

--- Source/racket_weather.rkt
+++ Source/racket_weather.rkt
@@ -8,11 +8,11 @@
 (get_weather cities_list get_list)
 
 ;; Generate the plots for all five cities
-(gen-graph list-of-json-files "Boston")                ;; Boston
-(gen-graph (cdr list-of-json-files) "Lawrence")        ;; Lawrence
-(gen-graph (cddr list-of-json-files) "Lowell")         ;; Lowell
-(gen-graph (cdddr list-of-json-files) "Worcester")     ;; Worcester
-(gen-graph (cddddr list-of-json-files) "Manchester")   ;; Manchester
+(print (gen-graph list-of-json-files "Boston"))                ;; Boston
+(print (gen-graph (cdr list-of-json-files) "Lawrence"))        ;; Lawrence
+(print (gen-graph (cddr list-of-json-files) "Lowell"))         ;; Lowell
+(print (gen-graph (cdddr list-of-json-files) "Worcester"))     ;; Worcester
+(print (gen-graph (cddddr list-of-json-files) "Manchester"))   ;; Manchester
 
 ;; Now try and run Huy's Viz program.
 ;; Use another require, and make another provide-all-defined
